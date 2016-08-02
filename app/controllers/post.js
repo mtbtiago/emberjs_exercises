7462fd35605f5e6aaa51a4705ddd5e9f677389bb
mtbtiago <tiago.ameller@gmail.com>
2016-08-02 21:19:20 +0200

    add actions to our controller

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    sayHello: function() {
+      alert('hello!');
+    },
+    toggleBody: function () {
+      this.toggleProperty('isShowingBody');
+    },
+    submitAction: function () {
+      alert(this.get('name'));
+      alert(this.get('comment'));
+    }
+  },
   title: 'My Blog Post',
   body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   authors: ['William', 'Robert', 'Michelle'],
@@ -17,5 +29,6 @@ export default Ember.Controller.extend({
       name: 'Manolo',
       comment: 'Nice job'
     }
-  ]
+  ],
+  isShowingBody: false
 });
