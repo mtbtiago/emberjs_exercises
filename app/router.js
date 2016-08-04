d1665c52e7e1db40e652b5e301a9feb4b398cefe
mtbtiago <tiago.ameller@gmail.com>
2016-08-04 20:55:31 +0200

    add users route, get user list from github api

--- app/router.js
+++ app/router.js
@@ -12,6 +12,7 @@ Router.map(function() {
   this.route('events');
   this.route('post');
   this.route('cars');
+  this.route('users');
 });
 
 export default Router;
---
+++ app/routes/users.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    var url = 'https://api.github.com/users';
+    return Ember.$.getJSON(url).then(
+      function(data) {
+        return data.splice(10,10);
+    });
+  }
+});
