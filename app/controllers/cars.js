199840b02971965cef03561fe120b65843e62d2b
mtbtiago <tiago.ameller@gmail.com>
2016-08-05 19:10:51 +0200

    deleting records

--- app/controllers/cars.js
+++ app/controllers/cars.js
@@ -1,4 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    deleteCar: function(id) {
+      var self = this;
+      this.store.findRecord('car', id).then(function(car) {
+        car.deleteRecord();
+        self.transitionToRoute('cars');
+      });
+    }
+  }
 });
